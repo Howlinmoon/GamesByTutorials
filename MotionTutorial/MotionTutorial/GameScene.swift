1d82a3b008409fffc0616c31d8f32b1533b6a2b7
Jim Veneskey <github@strifecrafter.com>
2015-10-09 06:15:56 -0400

    Basic left and right movement is working, need to add min and maximum to keep it on the screen

--- MotionTutorial/MotionTutorial/GameScene.swift
+++ MotionTutorial/MotionTutorial/GameScene.swift
@@ -7,39 +7,51 @@
 //
 
 import SpriteKit
+import CoreMotion
+
 
 class GameScene: SKScene {
+    
+    var airplane = SKSpriteNode()
+    var motionManager = CMMotionManager()
+    var destX:CGFloat = 0.0
+    
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+    
+        // 1
+        airplane = SKSpriteNode(imageNamed: "Airplane")
+        airplane.position = CGPointMake(frame.size.width/2, frame.size.height/2)
+        self.addChild(airplane)
         
-        self.addChild(myLabel)
+        
+        if motionManager.accelerometerAvailable == true {
+            // 2
+            motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue()!, withHandler:{
+                data, error in
+                
+                let currentX = self.airplane.position.x
+                
+                self.destX = currentX + CGFloat(data!.acceleration.x * 200)
+                
+            })
+            
+        } else {
+            print("This isn't going to do very much in the simulator, use a real device!\n")
+            self.destX = self.airplane.position.x        }
+    
+    
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        let action = SKAction.moveToX(destX, duration: 1.5)
+        self.airplane.runAction(action)
     }
 }
