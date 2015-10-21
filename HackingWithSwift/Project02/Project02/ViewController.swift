c1a0743028fd78d29fbe1d2d70f4a51b90a44e78
Jim Veneskey <github@strifecrafter.com>
2015-10-21 13:03:13 -0400

    Started Project 2, the Flag Game

---
+++ HackingWithSwift/Project02/Project02/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project02
+//
+//  Created by jim Veneskey on 10/21/15.
+//  Copyright © 2015 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
