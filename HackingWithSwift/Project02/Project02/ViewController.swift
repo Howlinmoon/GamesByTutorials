d30103f39354d238afd29c22601e24947554cc69
Howlinmoon <github@strifecrafter.com>
2015-10-22 09:04:24 -0400

    Programatically modifying the flags

--- HackingWithSwift/Project02/Project02/ViewController.swift
+++ HackingWithSwift/Project02/Project02/ViewController.swift
@@ -14,12 +14,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // initialize our array of country names
+countries = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        askQuestion()
     }
 
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), forState: .Normal)
+        button2.setImage(UIImage(named: countries[1]), forState: .Normal)
+        button3.setImage(UIImage(named: countries[2]), forState: .Normal)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
