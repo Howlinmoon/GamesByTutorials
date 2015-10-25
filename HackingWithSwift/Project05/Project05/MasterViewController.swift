4c4665194f2a906c9ca113b780ccb6c5e8f13264
Howlinmoon <github@strifecrafter.com>
2015-10-25 09:26:16 -0400

    Ran into yet another tutorial bug in project 5

--- HackingWithSwift/Project05/Project05/MasterViewController.swift
+++ HackingWithSwift/Project05/Project05/MasterViewController.swift
@@ -7,16 +7,37 @@
 //
 
 import UIKit
+import GameplayKit
 
 class MasterViewController: UITableViewController {
 
-    var objects = [AnyObject]()
+    var objects = [String]()
+    var allWords = [String]()
+    
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Read in our words
+        if let startWordsPath = NSBundle.mainBundle().pathForResource("start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath, usedEncoding: nil) {
+                allWords = startWords.componentsSeparatedByString("\n")
+            }
+        } else {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
     }
 
+    func startGame() {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(allWords) as! [String]
+        title = allWords[0]
+        objects.removeAll(keepCapacity: true)
+        tableView.reloadData()
+    }
+    
     override func viewWillAppear(animated: Bool) {
         self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
         super.viewWillAppear(animated)
@@ -40,8 +61,8 @@ class MasterViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object
         return cell
     }
 
