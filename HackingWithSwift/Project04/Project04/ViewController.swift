ca9e46f53c9c99ebf9550bb1112d6810b72b9d1e
Howlinmoon <github@strifecrafter.com>
2015-10-25 08:51:29 -0400

    Finished project 4, but it will not work due to a deprecated behavior.  Started project 5

--- HackingWithSwift/Project04/Project04/ViewController.swift
+++ HackingWithSwift/Project04/Project04/ViewController.swift
@@ -21,9 +21,36 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = NSURL(string: "https://www.hackingwithswift.com")!
+        // Initial web site
+//        let url = NSURL(string: "https://www.hackingwithswift.com")!
+        let url = NSURL(string: "https://www.google.com")!
         webView.loadRequest(NSURLRequest(URL: url))
         webView.allowsBackForwardNavigationGestures = true
+        
+        // Additions
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .Plain, target: self, action: "openTapped")
+        let spacer = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .Refresh, target: self, action: "refreshTapped")
+        
+        toolbarItems = [spacer, refresh]
+        navigationController?.toolbarHidden = false
+    }
+    
+    // New method called by tapping the 'Open' button
+    func openTapped() {
+        let ac = UIAlertController(title: "Open Page...", message: nil, preferredStyle: .ActionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .Default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .Default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+    }
+    
+    func openPage(action: UIAlertAction!) {
+        let url = NSURL(string: "https://" + action.title!)!
+        webView.loadRequest(NSURLRequest(URL: url))
+    }
+    
+    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+        title = webView.title
     }
 
     override func didReceiveMemoryWarning() {
