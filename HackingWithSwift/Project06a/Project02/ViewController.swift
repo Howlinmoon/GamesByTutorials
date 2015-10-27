915ef724399ec2c79e5c2074e23a0cf1abcfb473
Jim Veneskey <github@strifecrafter.com>
2015-10-27 06:12:56 -0400

    Starting project 6, a refactoring of project 2 with constraints

---
+++ HackingWithSwift/Project06a/Project02/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  Project02
+//
+//  Created by jim Veneskey on 10/21/15.
+//  Copyright © 2015 Jim Veneskey. All rights reserved.
+//
+
+import GameplayKit
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // initialize our array of country names
+countries = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        askQuestion()
+        
+        // give the buttons a border
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        // change the border color to gray
+        // .CGColor 'casts' the resulting UIColor into a CGColor...
+        button1.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button2.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button3.layer.borderColor = UIColor.lightGrayColor().CGColor
+        
+        
+    }
+
+    func askQuestion() {
+        // re-shuffle the array prior to selecting the first, second and third entries.
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(countries) as! [String]
+        // we are always selecting the first, second and third entries - but the source has been
+        // randomized, so the values we get are actually at random...
+        button1.setImage(UIImage(named: countries[0]), forState: .Normal)
+        button2.setImage(UIImage(named: countries[1]), forState: .Normal)
+        button3.setImage(UIImage(named: countries[2]), forState: .Normal)
+        // Select which is the 'correct' flag (randomizes 0, 1 or 2)
+        correctAnswer = GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
+        // Set our current title to the name of the correct flag's country
+        title = countries[correctAnswer].uppercaseString
+        
+    }
+    
+    
+    @IBAction func buttonTapped(sender: AnyObject) {
+    
+        var title: String
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score++
+        } else {
+            title = "WRONG!"
+            score--
+        }
+        
+        // Create an alert with the current score
+
+        // Modified the original code to use an example from udemy
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        ac.addAction((UIAlertAction(title: title, style: .Default, handler: { (action) -> Void in
+            self.dismissViewControllerAnimated(true, completion: nil)
+            self.askQuestion()
+            
+        })))
+        
+        self.presentViewController(ac, animated: true, completion: nil)
+
+        /*
+        original code
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .Alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .Default, handler: askQuestion))
+        
+        presentViewController(ac, animated: true, completion: nil)
+        */
+    }
+    
+    
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 Starting the Tutorial Series - Hacking With Swift
 
 Continue here:
-https://www.hackingwithswift.com/read/5/1/setting-up
+https://www.hackingwithswift.com/read/6/1/setting-up
 
-Project 5
+Project 6
 
 
