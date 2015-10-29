1b2d20981cace7450d60d0193759de3462d1558a
Jim Veneskey <github@strifecrafter.com>
2015-10-29 12:53:04 -0400

    Removed stock image, added new images, experimenting with rotating the background image

--- ZombieConga/ZombieConga/GameScene.swift
+++ ZombieConga/ZombieConga/GameScene.swift
@@ -10,5 +10,19 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
     backgroundColor = SKColor.blackColor()
+    
+    let background = SKSpriteNode(imageNamed: "background1")
+    // method #1 of setting the postion of the background image
+    // background.position = CGPoint(x: size.width/2, y: size.height/2)
+
+    // method #2
+    background.anchorPoint = CGPoint.zero
+    background.position = CGPoint.zero
+    
+    // Experimenting with rotation
+    background.zRotation = CGFloat(M_PI) / 8
+    
+    addChild(background)
+    
     }
 }
