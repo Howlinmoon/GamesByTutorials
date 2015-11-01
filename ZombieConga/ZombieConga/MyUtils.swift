5d861d1a56530e1ddebc27f3e7c78041b21d7814
Howlinmoon <github@strifecrafter.com>
2015-11-01 09:52:12 -0500

    Adding helper routines

---
+++ ZombieConga/ZombieConga/MyUtils.swift
@@ -0,0 +1,34 @@
+//
+//  MyUtils.swift
+//  ZombieConga
+//
+//  Created by Jim Veneskey on 11/1/15.
+//  Copyright © 2015 Jim Veneskey. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func += (inout left: CGPoint, right: CGPoint) {
+    left = left + right
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func -= (inout left: CGPoint, right: CGPoint) {
+    left = left - right
+}
+
+func * (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x * right.x, y: left.y * right.y)
+}
+
+func *= (inout left: CGPoint, right: CGPoint) {
+    left = left * right
+}
