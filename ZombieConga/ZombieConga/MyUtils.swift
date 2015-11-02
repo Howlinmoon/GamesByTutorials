53c6c798eae2922c62c981cc9307406f9a964197
Jim Veneskey <github@strifecrafter.com>
2015-11-02 06:01:14 -0500

    All set for chapter 3

--- ZombieConga/ZombieConga/MyUtils.swift
+++ ZombieConga/ZombieConga/MyUtils.swift
@@ -32,3 +32,51 @@ func * (left: CGPoint, right: CGPoint) -> CGPoint {
 func *= (inout left: CGPoint, right: CGPoint) {
     left = left * right
 }
+
+func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func *= (inout point: CGPoint, scalar: CGFloat) {
+    point = point * scalar
+}
+
+func / (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x / right.x, y: left.y / right.y)
+}
+
+func /= (inout left: CGPoint, right: CGPoint) {
+    left = left / right
+}
+
+func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x / scalar , y: point.y / scalar)
+}
+
+func /= (inout point: CGPoint, scalar: CGFloat) {
+    point = point / scalar
+}
+
+#if !(arch(x86_64) || arch(arm64))
+func atan2(y: CGFloat, x: CGFloat) -> CGFloat {
+    return CGFloat(atan2f(Float(y), Float(x)))
+}
+
+    func sqrt(a: CGFloat) -> CGFloat {
+        return CGFloat(sqrtf(Float(a)))
+}
+#endif
+
+extension CGPoint {
+    func length() -> CGFloat {
+        return sqrt(x*x + y*y)
+    }
+    
+    func normalized() -> CGPoint {
+        return self / length()
+    }
+    
+    var angle: CGFloat {
+        return atan2(y, x)
+    }
+}
